68abb23edcc3e43d3cfe9842a684221da96e5da1
Thejo Varma Konduru <thejo@Thejos-MacBook-Air.local>
2023-05-05 16:22:21 +0530

    Added an address

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -82,6 +82,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[
+    "remote.getryt.in",
     "rs-ny.rustdesk.com",
     "rs-sg.rustdesk.com",
     "rs-cn.rustdesk.com",
